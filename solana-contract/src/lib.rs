03f91a85c0a26344b9f37732f1649af5744e1dd1
gotoswap <2nb2nb@gmail.com>
2023-10-19 06:03:16 +0800

    update

--- solana-contract/src/lib.rs
+++ solana-contract/src/lib.rs
@@ -1,22 +1,25 @@
+#![feature(int_roundings)]
+
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::program::invoke;
 use anchor_lang::solana_program::system_instruction;
 
-declare_id!("5GQn3NDZgciJmNB85Az5V5FrAY64QddUWW9kKsRDLn2a");
+declare_id!("4HzFUvrz1bZTVNHMVaBgXmJbfiPpxCX3VpH4YH7FyvvB");
 
-const PREFIX_STATE: &str= "state";
+const PREFIX_STATE: &str = "state";
 const PREFIX_CONFIG: &str = "config";
 const PREFIX_BALANCE: &str = "balance";
 const PREFIX_ORDER: &str = "order";
 const PREFIX_RECEIPT: &str = "receipt";
 const PREFIX_CCFEE: &str = "ccfee";
+const PREFIX_COLLATERAL: &str = "collateral";
+const PREFIX_WITNESSES: &str = "witnesses";
 const ADMIN: &str = "BuTuA7YKzx5CUn3bALZcK97jQrFM94QfsBUaUdM6BCxm";
 
 #[program]
 pub mod solana_lending {
     use super::*;
 
-    #[access_control(only_admin(&ctx.accounts.payer))]
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         let global = &mut ctx.accounts.global_state;
         global.curr_order_sn = 1;
@@ -115,7 +118,7 @@ pub mod solana_lending {
 
     pub fn close_order(ctx: Context<CloseOrder>, order_sn: u64) -> Result<()> {
         let order = &ctx.accounts.order;
-        require!(order.balance == 0, MyError::NonZeroOrderBalance);
+        require!(order.balance == 0, MyError::CloseOrderWithNonZeroBalance);
         emit!(EventCloseOrder {
             order_sn: order_sn,
             lender: order.lender,
@@ -123,13 +126,69 @@ pub mod solana_lending {
         Ok(())
     }
 
-    #[access_control(only_admin(&ctx.accounts.payer))]
-    pub fn borrow(ctx: Context<Borrow>, order_sn: u64, borrower: Pubkey, amount: u64, chainid: u32, c_sn: u64, source: [u8; 20], token: [u8; 20], frozen: u64) -> Result<()> {
-        let receipt = &mut ctx.accounts.receipt;
+    #[access_control(is_witness(&ctx.accounts.payer, &ctx.accounts.witnesses))]
+    pub fn multisig_collateral(ctx: Context<MultisigCollateral>, chainid: u32, collateral_sn: u64, source: [u8; 20], token: [u8; 20], frozen: u64, order_sn: u64, borrower: Pubkey, amount: u64) -> Result<()> {
+        let multisig = &mut ctx.accounts.multisig;
+        let witnesses = &mut ctx.accounts.witnesses;
+        let payer = &ctx.accounts.payer;
+        let collateral = Collateral {
+            chainid,
+            collateral_sn,
+            source,
+            token,
+            frozen,
+            borrower,
+            order_sn,
+            amount,
+        };
+        match multisig.status {
+            MultisigStatus::Starting => {
+                let mut branches = Vec::new();
+                branches.push(CollateralWithSigners {
+                    collateral,
+                    signers: vec![payer.key()], 
+                });
+                multisig.branches = branches;
+                multisig.status = MultisigStatus::InProgress;
+            }
+            MultisigStatus::InProgress => {
+                for branch in multisig.branches.iter() {
+                    if let Some(_) = branch.signers.iter().find(|s| s == &payer.key) {
+                        return Err(MyError::AlreadySigned.into());
+                    }
+                }
+                match multisig.branches.iter_mut().find(|b| b.collateral == collateral) {
+                    Some(branch) => {
+                        branch.signers.push(payer.key());
+                        if branch.signers.len() == witnesses.multisig_threshold {
+                            multisig.status = MultisigStatus::ReachConsensus;
+                            multisig.branches.retain(|b| b.signers.len() == witnesses.multisig_threshold);
+                        }
+                    }
+                    None => {
+                        multisig.branches.push(CollateralWithSigners {
+                            collateral,
+                            signers: vec![payer.key()],
+                        });
+                    }
+                }
+            }
+            _ => {}
+        }
+        Ok(())
+    }
+
+    pub fn borrow(ctx: Context<Borrow>, _chainid: u32, _collateral_sn: u64) -> Result<()> {
+        let multisig = &mut ctx.accounts.multisig;
         let order = &mut ctx.accounts.order;
+        let receipt = &mut ctx.accounts.receipt;
         let global = &mut ctx.accounts.global;
-        require!(order.balance >= amount, MyError::InsufficientOrderBalance);
         
+        require!(multisig.status == MultisigStatus::ReachConsensus, MyError::CannotBorrowWithoutConsensus);
+        let Collateral { chainid, collateral_sn: c_sn, source, token, frozen, borrower, order_sn, amount } = multisig.branches[0].collateral;
+        require!(order.balance >= amount, MyError::InsufficientOrderBalance);
+        multisig.status = MultisigStatus::Finished;
+
         receipt.sn = global.curr_receipt_sn;
         receipt.borrower = borrower;
         receipt.lender = order.lender;
@@ -146,22 +205,29 @@ pub mod solana_lending {
 
         emit!(EventBorrowSuccess {
             receipt_sn: receipt.sn,
-            borrower: borrower,
+            borrower,
             lender: receipt.lender,
-            source: source,
-            chainid: chainid,
-            c_sn: c_sn,
-            token: token,
-            frozen: frozen,
-            amount: amount,
+            source,
+            chainid,
+            c_sn,
+            token,
+            frozen,
+            amount,
             time: receipt.time,
             rate: receipt.rate,
-            order_sn: order_sn,
+            order_sn,
             order_balance: order.balance,
         });
 
         **ctx.accounts.order.to_account_info().try_borrow_mut_lamports()? -= amount;
         **ctx.accounts.recipient.try_borrow_mut_lamports()? += amount;
+        Ok(())
+    }
+
+    pub fn clear_collateral(ctx: Context<ClearCollateral>, _chainid: u32, _collateral_sn: u64) -> Result<()> {
+        let multisig = &mut ctx.accounts.multisig;
+        require!(multisig.status == MultisigStatus::Finished, MyError::CannotClearUnfinishedMultisig);
+        multisig.branches.clear();
         Ok(())
     }
 
@@ -244,6 +310,37 @@ pub mod solana_lending {
         ccfee.fee = fee;
         Ok(())
     }
+
+    #[access_control(only_admin(&ctx.accounts.payer))]
+    pub fn add_witness(ctx: Context<UpdateWitnesses>, witness: Pubkey) -> Result<()> {
+        let witnesses = &mut ctx.accounts.witnesses;
+        if let Some(_) = witnesses.v.iter().find(|&&w| w == witness) {
+            return Err(MyError::WitnessAlreadyExist.into());   
+        }
+        witnesses.v.push(witness);
+        witnesses.multisig_threshold = (witnesses.v.len() * 2).div_ceil(3);
+        Ok(())
+    }
+
+    #[access_control(only_admin(&ctx.accounts.payer))]
+    pub fn remove_witness(ctx: Context<UpdateWitnesses>, witness: Pubkey) -> Result<()> {
+        let witnesses = &mut ctx.accounts.witnesses;
+        witnesses.v.retain(|&w| w != witness);
+        witnesses.multisig_threshold = (witnesses.v.len() * 2).div_ceil(3);
+        Ok(())
+    }
+}
+
+pub fn only_admin<'info>(payer: &Signer<'info>) -> Result<()> {
+    if payer.key.to_string() != ADMIN {
+        return Err(MyError::NoOperationPermission.into());
+    }
+    Ok(())
+}
+
+pub fn is_witness<'info>(payer: &Signer<'info>, witnesses: &Account<'info, Witnesses>) -> Result<()> {
+    witnesses.v.iter().find(|&&w| w == payer.key()).ok_or(MyError::NoOperationPermission)?;
+    Ok(())
 }
 
 #[derive(Accounts)]
@@ -305,16 +402,18 @@ pub struct CloseOrder<'info> {
 }
 
 #[derive(Accounts)]
-#[instruction(order_sn: u64, borrower: Pubkey)]
+#[instruction(chainid: u32, collateral_sn: u64)]
 pub struct Borrow<'info> {
+    #[account(mut, seeds = [PREFIX_COLLATERAL.as_bytes(), chainid.to_le_bytes().as_ref(), collateral_sn.to_le_bytes().as_ref()], bump)]
+    pub multisig: Box<Account<'info, CollateralWithMultisig>>,
     #[account(init, payer = payer, space = 8 + 156, seeds = [PREFIX_RECEIPT.as_bytes(), global.curr_receipt_sn.to_le_bytes().as_ref()], bump)]
     pub receipt: Account<'info, LoanReceipt>,
-    #[account(mut, seeds = [PREFIX_ORDER.as_bytes(), order_sn.to_le_bytes().as_ref()], bump)]
+    #[account(mut, seeds = [PREFIX_ORDER.as_bytes(), multisig.branches[0].collateral.order_sn.to_le_bytes().as_ref()], bump)]
     pub order: Account<'info, Order>,
     #[account(mut, seeds = [PREFIX_STATE.as_bytes()], bump)]
     pub global: Account<'info, GlobalState>,
     /// CHECK: 
-    #[account(mut, constraint = *recipient.key == borrower)]
+    #[account(mut, constraint = *recipient.key == multisig.branches[0].collateral.borrower)]
     pub recipient: AccountInfo<'info>,
     #[account(mut)]
     pub payer: Signer<'info>,
@@ -379,17 +478,86 @@ pub struct SetConfig<'info> {
 }
 
 #[derive(Accounts)]
-#[instruction(_chainid: u32)]
+#[instruction(chainid: u32)]
 pub struct SetCCFee<'info> {
-    #[account(init_if_needed, payer = payer, space = 8 + 8, seeds = [PREFIX_CCFEE.as_bytes(), _chainid.to_le_bytes().as_ref()], bump)]
+    #[account(init_if_needed, payer = payer, space = 8 + 8, seeds = [PREFIX_CCFEE.as_bytes(), chainid.to_le_bytes().as_ref()], bump)]
     pub cc_fee: Account<'info, CCFee>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+   
+#[derive(Accounts)]
+#[instruction(chainid: u32, collateral_sn: u64)]
+pub struct MultisigCollateral<'info> {
+    #[account(init_if_needed, payer = payer, space = 8 + 1024, seeds = [PREFIX_COLLATERAL.as_bytes(), chainid.to_le_bytes().as_ref(), collateral_sn.to_le_bytes().as_ref()], bump)]
+    pub multisig: Box<Account<'info, CollateralWithMultisig>>,
+    #[account(seeds = [PREFIX_WITNESSES.as_bytes()], bump)]
+    pub witnesses: Account<'info, Witnesses>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(chainid: u32, collateral_sn: u64)]
+pub struct ClearCollateral<'info> {
+    #[account(mut, seeds = [PREFIX_COLLATERAL.as_bytes(), chainid.to_le_bytes().as_ref(), collateral_sn.to_le_bytes().as_ref()], bump, 
+        realloc = 8 + 5, realloc::payer = payer, realloc::zero = false)]
+    pub multisig: Box<Account<'info, CollateralWithMultisig>>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateWitnesses<'info> {
+    #[account(init_if_needed, payer = payer, space = 8 + 1024, seeds = [PREFIX_WITNESSES.as_bytes()], bump)]
+    pub witnesses: Box<Account<'info, Witnesses>>,
     #[account(mut)]
     pub payer: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
 #[account]
-#[derive(Default)]
+pub struct CollateralWithMultisig {
+    pub status: MultisigStatus,
+    pub branches: Vec<CollateralWithSigners>, 
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, PartialEq, Copy, Clone)]
+pub enum MultisigStatus {
+    Starting,
+    InProgress,
+    ReachConsensus,
+    Finished,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, PartialEq, Copy, Clone)]
+pub struct Collateral {
+    pub chainid: u32,
+    pub collateral_sn: u64,
+    pub source: [u8; 20],
+    pub token: [u8; 20],
+    pub frozen: u64,
+    pub borrower: Pubkey,
+    pub order_sn: u64,
+    pub amount: u64,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct CollateralWithSigners {
+    pub collateral: Collateral,
+    pub signers: Vec<Pubkey>,
+}
+
+#[account]
+pub struct Witnesses {
+    pub multisig_threshold: usize,
+    pub v: Vec<Pubkey>,
+}
+
+#[account]
 pub struct LoanReceipt {
     pub sn: u64,
     pub borrower: Pubkey,
@@ -405,7 +573,6 @@ pub struct LoanReceipt {
 }
 
 #[account]
-#[derive(Default)]
 pub struct Order {
     pub sn: u64,
     pub lender: Pubkey,
@@ -414,20 +581,17 @@ pub struct Order {
 }
 
 #[account]
-#[derive(Default)]
 pub struct UserBalance {
     pub amount: u64,
 }
 
 #[account]
-#[derive(Default)]
 pub struct GlobalState {
     pub curr_order_sn: u64,
     pub curr_receipt_sn: u64,
 }
 
 #[account]
-#[derive(Default)]
 pub struct Config {
     pub min_ir: u64,
     pub max_ir: u64,
@@ -439,7 +603,6 @@ pub struct Config {
 }
 
 #[account]
-#[derive(Default)]
 pub struct CCFee {
     pub fee: u64,
 }
@@ -508,13 +671,6 @@ pub struct EventLiquidate {
     pub receiver: [u8; 20],
 }
 
-pub fn only_admin<'info>(payer: &Signer<'info>) -> Result<()> {
-    if payer.key.to_string() != ADMIN {
-        return Err(MyError::NoOperationPermission.into());
-    }
-    Ok(())
-}
-
 #[error_code]
 pub enum MyError {
     #[msg("insufficient user balance")]
@@ -523,8 +679,8 @@ pub enum MyError {
     IllegalInterestRate,
     #[msg("no order found")]
     NoOrderFound,
-    #[msg("non-zero order balance")]
-    NonZeroOrderBalance,
+    #[msg("close an order with non-zero balance")]
+    CloseOrderWithNonZeroBalance,
     #[msg("insufficient order balance")]
     InsufficientOrderBalance,
     #[msg("no operation permission")]
@@ -533,4 +689,12 @@ pub enum MyError {
     NoReceiptFound,
     #[msg("deadline not meet")]
     DeadlineNotMeet,
+    #[msg("already signed")]
+    AlreadySigned,
+    #[msg("cannot borrow without consensus")]
+    CannotBorrowWithoutConsensus,
+    #[msg("cannot clear unfinished multisig account")]
+    CannotClearUnfinishedMultisig,
+    #[msg("witness already exist")]
+    WitnessAlreadyExist,
 }
